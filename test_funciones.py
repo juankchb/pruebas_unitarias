6fdb9e5fbcda3bcab992d9e4856451a6761ade14
Ubuntu <ubuntu@ip-172-31-23-119.ec2.internal>
2023-02-17 20:26:23 +0000

    initial

---
+++ test_funciones.py
@@ -0,0 +1,10 @@
+from functions import sumar,isitPrime
+
+def test_sumar():
+    assert sumar(2,4)==6
+    assert sumar(-2,2)==0
+    assert sumar(2,2)==4
+    
+def test_isitprime():
+    assert isitPrime(7) is True
+    assert isitPrime(4) is False
